97167bfe588e63cd26e1d1da386e5aa40b6646d8
Louis Grasset <louisgrasset@protonmail.com>
2023-08-23 18:21:34 +0200

    feat: remove proxy

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -55,27 +55,7 @@ export const configuration = async (): Promise<{
     });
     synchronizedHandle.set(`@${TWITTER_HANDLE}`);
 
-    const twitterClient = new Scraper({
-        transform: {
-            request(input: RequestInfo | URL, init?: RequestInit) {
-                // The arguments here are the same as the parameters to fetch(), and
-                // are kept as-is for flexibility of both the library and applications.
-                if (input instanceof URL) {
-                    const proxy =
-                        'https://corsproxy.org/?' +
-                        encodeURIComponent(input.toString());
-                    return [proxy, init];
-                } else if (typeof input === 'string') {
-                    const proxy =
-                        'https://corsproxy.org/?' + encodeURIComponent(input);
-                    return [proxy, init];
-                } else {
-                    // Omitting handling for example
-                    throw new Error('Unexpected request input type');
-                }
-            },
-        },
-    });
+    const twitterClient = new Scraper();
 
     await handleTwitterAuth(twitterClient);
 
