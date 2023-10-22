30e22571a39a940353710854eeef5dd19b2f0634
Louis Grasset <louisgrasset@protonmail.com>
2023-10-22 18:45:09 +0200

    test(helpers): add first test

---
+++ src/helpers/tweet/__tests__/format-tweet-text.spec.ts
@@ -0,0 +1,14 @@
+import { Tweet } from '@the-convocation/twitter-scraper';
+
+import { formatTweetText } from '../format-tweet-text.js';
+
+describe('formatTweetText', () => {
+    it('should keep untouched a tweet without links', () => {
+        const tweet: Tweet = {
+            text:'This is a tweet without links.',
+            urls: []
+        } as unknown as Tweet;
+        const result = formatTweetText(tweet);
+        expect(result).toStrictEqual('This is a tweet without links.');
+    });
+});
