8a1aadb3ccc499505cc96df0903594be1ff7e06f
Louis Grasset <louisgrasset@protonmail.com>
2023-10-23 01:18:59 +0200

    test(tweet): cover helpers

---
+++ src/helpers/tweet/__tests__/is-tweet-cached.spec.ts
@@ -0,0 +1,21 @@
+import { Tweet } from "@the-convocation/twitter-scraper";
+
+import { isTweetCached } from "../is-tweet-cached.js";
+
+describe("isTweetCached", () => {
+  it("should return true if the tweet is cached", () => {
+    const result = isTweetCached(
+      { id: "1234567890123456789" } as unknown as Tweet,
+      {
+        "1234567890123456789": {
+          mastodon: "mastodonId",
+          bluesky: {
+            cid: "cid",
+            rkey: "rkey",
+          },
+        },
+      },
+    );
+    expect(result).toBe(true);
+  });
+});
---
+++ src/helpers/tweet/__tests__/keep-self-replies.spec.ts
@@ -0,0 +1,37 @@
+import { Tweet } from "@the-convocation/twitter-scraper";
+
+import { keepSelfReplies } from "../keep-self-replies.js";
+
+jest.mock("../../../constants.js", () => {
+  return {
+    TWITTER_HANDLE: "username",
+  };
+});
+
+describe("keepSelfReplies", () => {
+  describe("when the tweet is a reply", () => {
+    it("should return true when is to the same user", async () => {
+      const result = await keepSelfReplies({
+        inReplyToStatus: { username: "username" },
+      } as unknown as Tweet);
+
+      expect(result).toBe(true);
+    });
+
+    it("should return false when is to a different user", async () => {
+      const result = await keepSelfReplies({
+        inReplyToStatus: { username: "potatoinc" },
+      } as unknown as Tweet);
+
+      expect(result).toBe(false);
+    });
+  });
+
+  describe("when the tweet is not a reply", () => {
+    it("should return true when is from the same user", async () => {
+      const result = await keepSelfReplies({} as unknown as Tweet);
+
+      expect(result).toBe(true);
+    });
+  });
+});
