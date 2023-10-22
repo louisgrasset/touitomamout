3c8e730d2452d04b478f0d52a872e0e59b91b663
Louis Grasset <louisgrasset@users.noreply.github.com>
2023-10-22 18:47:38 +0200

    feat(test): add jest (#84)

---
+++ meta/testing/jest.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+    collectCoverageFrom: ['src/**/*.ts'],
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    modulePaths: ['<rootDir>/src/'],
+    rootDir: '../../',
+    setupFilesAfterEnv: ['<rootDir>/meta/testing/jest.setup.cjs'],
+    testRegex: '(/__tests__/.*\\.spec)\\.ts?$',
+    testTimeout: 10000,
+    transform: {
+        "^.+\\.ts?$": ["ts-jest", {
+            useESM: true,
+        }]
+    },
+    verbose: false,
+};
