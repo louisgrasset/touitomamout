2b96ee9992379d9e059661fba19cf16574171a65
Louis Grasset <louisgrasset@protonmail.com>
2023-11-01 13:17:08 +0100

    fix(split-tweet-text): extract methods; simplify cognitive load

--- src/helpers/tweet/split-tweet-text.ts
+++ src/helpers/tweet/split-tweet-text.ts
@@ -2,58 +2,23 @@ import { Tweet } from "@the-convocation/twitter-scraper";
 
 import {
   BLUESKY_MAX_POST_LENGTH,
-  MASTODON_INSTANCE,
   MASTODON_MAX_POST_LENGTH,
 } from "../../constants.js";
-import { MastodonCacheChunk, Platform } from "../../types/index.js";
-import { getCachedPostChunk } from "../post/get-cached-post-last-chunk.js";
+import { Platform } from "../../types/index.js";
+import { getMastodonQuoteLinkSection } from "./split-tweet-text/get-mastodon-quote-link-section.js";
 
-export const splitTextForMastodon = (tweet: Tweet, mastodonUsername: string) =>
-  splitTweetText(tweet, Platform.MASTODON, mastodonUsername);
-export const splitTextForBluesky = (tweet: Tweet) =>
-  splitTweetText(tweet, Platform.BLUESKY);
-
-const splitTweetText = async (
-  { text, quotedStatusId }: Tweet,
+const buildChunks = (
+  text: string | undefined,
   platform: Platform,
-  mastodonUsername?: string,
-): Promise<string[]> => {
-  const maxChunkSize =
-    platform === Platform.MASTODON
-      ? MASTODON_MAX_POST_LENGTH
-      : BLUESKY_MAX_POST_LENGTH;
-
-  const quoteData = await getCachedPostChunk<MastodonCacheChunk>(
-    quotedStatusId,
-    Platform.MASTODON,
-    "last",
-  );
-  const quotedStatusLinkSection = `\n\nhttps://${MASTODON_INSTANCE}/@${
-    mastodonUsername ?? ""
-  }/${quoteData}`;
-
-  // Small post optimization
-  if (quotedStatusId) {
-    if (platform === Platform.MASTODON) {
-      // Specific optimization for Mastodon (it has to include a link to the quoted status)
-      if (text!.length - quotedStatusLinkSection.length <= maxChunkSize) {
-        return [text + quotedStatusLinkSection];
-      }
-    }
-  } else {
-    if (text!.length <= maxChunkSize) {
-      return [text!];
-    }
-  }
-
-  // Splitting the post
+  quotedStatusId: string | undefined,
+  maxChunkSize: number,
+  quotedStatusLinkSection: string,
+) => {
   const words = text!.split(" ");
   const chunks: string[] = [];
   let currentChunk = "";
 
-  for (let i = 0; i < words.length; i++) {
-    const word = words[i];
-
+  for (const word of words) {
     const currentChunkWithAddedWord = (currentChunk + " " + word).trim();
     const shouldAppendQuoteLink =
       chunks.length === 0 && platform === Platform.MASTODON && !!quotedStatusId;
@@ -79,3 +44,47 @@ const splitTweetText = async (
 
   return chunks;
 };
+
+const splitTweetText = async (
+  { text, quotedStatusId }: Tweet,
+  platform: Platform,
+  mastodonUsername?: string,
+): Promise<string[]> => {
+  const maxChunkSize =
+    platform === Platform.MASTODON
+      ? MASTODON_MAX_POST_LENGTH
+      : BLUESKY_MAX_POST_LENGTH;
+
+  const quotedStatusLinkSection = await getMastodonQuoteLinkSection(
+    quotedStatusId,
+    mastodonUsername,
+  );
+
+  // Small post optimization
+  if (quotedStatusId) {
+    if (platform === Platform.MASTODON) {
+      // Specific optimization for Mastodon (it has to include a link to the quoted status)
+      if (text!.length - quotedStatusLinkSection.length <= maxChunkSize) {
+        return [text + quotedStatusLinkSection];
+      }
+    }
+  } else {
+    if (text!.length <= maxChunkSize) {
+      return [text!];
+    }
+  }
+
+  return buildChunks(
+    text,
+    platform,
+    quotedStatusId,
+    maxChunkSize,
+    quotedStatusLinkSection,
+  );
+};
+
+export const splitTextForMastodon = (tweet: Tweet, mastodonUsername: string) =>
+  splitTweetText(tweet, Platform.MASTODON, mastodonUsername);
+
+export const splitTextForBluesky = (tweet: Tweet) =>
+  splitTweetText(tweet, Platform.BLUESKY);
