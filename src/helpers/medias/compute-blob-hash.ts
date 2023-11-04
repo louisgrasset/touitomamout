c075f0c6ce76bddc26b3ea1b52501272152c7143
Louis Grasset <louisgrasset@protonmail.com>
2023-11-04 02:52:31 +0100

    fix(crypto): add import

--- src/helpers/medias/compute-blob-hash.ts
+++ src/helpers/medias/compute-blob-hash.ts
@@ -1,8 +1,10 @@
+import { subtle } from "crypto";
+
 import { DEBUG } from "../../constants.js";
 
 export const computeBlobHash = async (blob: Blob): Promise<string> => {
   const buffer = await blob.arrayBuffer();
-  const hashBuffer = await crypto.subtle.digest("SHA-256", buffer);
+  const hashBuffer = await subtle.digest("SHA-256", buffer);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
   const hash = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
 
