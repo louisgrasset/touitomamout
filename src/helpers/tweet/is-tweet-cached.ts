b09fd5fedaf861ac7490bbb228efcb8781dd05aa
Louis Grasset <louisgrasset@protonmail.com>
2023-11-04 01:37:06 +0100

    chore: reduce import

--- src/helpers/tweet/is-tweet-cached.ts
+++ src/helpers/tweet/is-tweet-cached.ts
@@ -1,6 +1,6 @@
 import { Tweet } from "@the-convocation/twitter-scraper";
 
-import { PostsCache } from "../../types/postsCache.js";
+import { PostsCache } from "../../types/index.js";
 
 export const isTweetCached = (tweet: Tweet, cache: PostsCache) =>
   !!cache[tweet.id ?? 0];
