c4df1e90f349af1ceae0fce864445e830a04ec4f
Louis Grasset <louisgrasset@protonmail.com>
2023-11-19 17:51:23 +0100

    test(bluesky): support link metadata

---
+++ src/helpers/bluesky/__tests__/mocks/metadata.ts
@@ -0,0 +1,11 @@
+export const METADATA_MOCK = {
+  error: "",
+  likely_type: "html",
+  url: "https://github.com/louisgrasset/touitomamout",
+  title:
+    "GitHub - louisgrasset/touitomamout: Touitomamout is an easy way to synchronize your Twitter's tweets...",
+  description:
+    "Touitomamout is an easy way to synchronize your Twitter\u0026#39;s tweets 🦤 to Mastodon 🦣 and Bluesky post ☁️ (also known as Twitter to Mastodon \u0026amp; Bluesky crossposter) - GitHub - louisgrasset...",
+  image:
+    "https://cardyb.bsky.app/v1/image?url=https%3A%2F%2Fopengraph.githubassets.com%2Fc62e7ca13ff991d5b45a3218958a7bfefc990ee3386bd75fee3c6159e91c8859%2Flouisgrasset%2Ftouitomamout",
+};
