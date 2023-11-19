c543b1208e24dca979b22015a5a524805a979d8d
Louis Grasset <louisgrasset@protonmail.com>
2023-11-19 19:39:05 +0100

    fix(quote): prevent quoting unavailable tweet

--- src/helpers/tweet/keep-self-quotes.ts
+++ src/helpers/tweet/keep-self-quotes.ts
@@ -3,8 +3,10 @@ import { Tweet } from "@the-convocation/twitter-scraper";
 import { TWITTER_HANDLE } from "../../constants.js";
 
 export const keepSelfQuotes = async (tweet: Tweet) => {
-  if (tweet.quotedStatus) {
-    return tweet.quotedStatus.username === TWITTER_HANDLE;
+  if (tweet.isQuoted) {
+    return tweet.quotedStatus
+      ? tweet.quotedStatus.username === TWITTER_HANDLE // If the quoted tweet is from the same user, keep it.
+      : false; // If the quoted tweet is not available, skip the tweet.
   }
 
   // True by default so chained conditions works
