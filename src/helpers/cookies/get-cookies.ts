b8b0f8a4411209ab8f06cd310971ce070e215d84
Louis Grasset <louisgrasset@protonmail.com>
2023-11-23 00:30:19 +0100

    chore(cookies): fix import creating warns in coverage generation

--- src/helpers/cookies/get-cookies.ts
+++ src/helpers/cookies/get-cookies.ts
@@ -1,11 +1,11 @@
-import fs from "fs";
+import { readFile } from "fs/promises";
 import { Cookie } from "tough-cookie";
 
 import { COOKIES_PATH } from "../../constants.js";
 
 export const getCookies = async (): Promise<Cookie[] | null> => {
   try {
-    const fileContent = await fs.promises.readFile(COOKIES_PATH, "utf-8");
+    const fileContent = await readFile(COOKIES_PATH, "utf-8");
 
     return Object.values(JSON.parse(fileContent)).reduce((acc: Cookie[], c) => {
       const cookie = Cookie.fromJSON(JSON.stringify(c));
