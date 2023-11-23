7308533843d7132406049dd34b89f4ec8eb10b8d
Louis Grasset <louisgrasset@protonmail.com>
2023-11-23 09:12:24 +0100

    test(services): cover mediaDownloaderService

---
+++ src/services/__tests__/media-downloader.service.spec.ts
@@ -0,0 +1,16 @@
+import { mediaDownloaderService } from "../media-downloader.service.js";
+
+describe("mediaDownloaderService", () => {
+  it("should download media", async () => {
+    const url = "https://sample-videos.com/img/Sample-png-image-100kb.png";
+    const result = await mediaDownloaderService(url);
+
+    expect(result).toBeInstanceOf(Blob);
+  });
+
+  describe("when the media is not found", () => {
+    it("should fail", async () => {
+      await expect(mediaDownloaderService("")).rejects.toBeInstanceOf(Error);
+    });
+  });
+});
