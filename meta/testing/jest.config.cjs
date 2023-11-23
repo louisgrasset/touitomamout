0d7ae6d77524264b1321a1371157970dd1604b62
Louis Grasset <louisgrasset@protonmail.com>
2023-11-23 03:22:26 +0100

    ci(ts-jest): rely on `ts-jest/presets/default-esm` preset

--- meta/testing/jest.config.cjs
+++ meta/testing/jest.config.cjs
@@ -11,14 +11,7 @@ module.exports = {
     setupFilesAfterEnv: ['<rootDir>/meta/testing/jest.setup.cjs'],
     testRegex: '(/__tests__/.*\\.spec)\\.ts?$',
     testTimeout: 10000,
-    transform: {
-        "^.+\\.ts?$": ["ts-jest", {
-            useESM: true,
-        }],
-        "^.+\\.js?$": ["ts-jest", {
-            useESM: true,
-        }]
-    },
+    preset: "ts-jest/presets/default-esm",
     verbose: false,
     maxWorkers: "50%"
 };
