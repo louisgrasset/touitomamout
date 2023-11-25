9a0e5ce6515107a838240ae9307f893af2073b66
Louis Grasset <louisgrasset@protonmail.com>
2023-11-25 21:25:59 +0100

    test(ora): enhance mock

--- src/__tests__/__mocks__/ora.ts
+++ src/__tests__/__mocks__/ora.ts
@@ -1,13 +1,19 @@
+const methods = {
+  fail: jest.fn(),
+  info: jest.fn(),
+  stop: jest.fn(),
+  succeed: jest.fn(),
+  warn: jest.fn(),
+};
+
 module.exports = {
   name: "ora",
   default: jest.fn(() => ({
-    start: jest
-      .fn()
-      .mockImplementation(() => ({ text: "", succeed: jest.fn() })),
-    succeed: jest.fn(),
-    fail: jest.fn(),
-    info: jest.fn(),
-    warn: jest.fn(),
+    start: jest.fn().mockImplementation(() => ({
+      text: "",
+      ...methods,
+    })),
+    ...methods,
   })),
   __esModule: true,
 };
