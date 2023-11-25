1ab1c29504fcf140294bfb1a9980e717ce6ca0e3
Louis Grasset <louisgrasset@protonmail.com>
2023-11-25 21:34:37 +0100

    test(configuration): cover buildConfigurationRules

---
+++ src/configuration/__tests__/build-configuration-rules.spec.ts
@@ -0,0 +1,21 @@
+import { buildConfigurationRules } from "../build-configuration-rules.js";
+
+jest.mock("../../constants.js", () => ({
+  BLUESKY_IDENTIFIER: "username",
+  BLUESKY_INSTANCE: "bsky.social",
+  BLUESKY_PASSWORD: "app-password",
+  MASTODON_ACCESS_TOKEN: "access-token",
+  MASTODON_INSTANCE: "mastodon.social",
+  SYNC_BLUESKY: true,
+  SYNC_MASTODON: true,
+  TWITTER_HANDLE: "username",
+}));
+
+describe("buildConfigurationRules", () => {
+  it("should return an array of configuration rules", () => {
+    const result = buildConfigurationRules();
+    expect(result).toBeDefined();
+    expect(result).toBeInstanceOf(Array);
+    expect(result).toHaveLength(6);
+  });
+});
