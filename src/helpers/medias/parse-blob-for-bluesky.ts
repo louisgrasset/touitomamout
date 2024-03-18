ff9d3b99b44221798f8ca24ab8ae12f5502402f0
Joshua Holme <joshuaholme19@icloud.com>
2024-03-18 00:28:58 -0400

    add webp support

--- src/helpers/medias/parse-blob-for-bluesky.ts
+++ src/helpers/medias/parse-blob-for-bluesky.ts
@@ -19,7 +19,7 @@ export const parseBlobForBluesky = async (
   ).catch(() => inputBlob);
 
   return new Promise<BlueskyBlob>((resolve, reject) => {
-    const allowedMimeTypes = ["image/jpeg", "image/jpg", "image/png"];
+    const allowedMimeTypes = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
     const mimeType = blob.type;
 
     blob.arrayBuffer().then((ab) => {
