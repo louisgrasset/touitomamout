7057a046b84345f651cb5d5d1d466b128b78df45
Louis Grasset <louisgrasset@protonmail.com>
2024-05-24 22:38:36 +0200

    test(bluesky): update expectations

--- src/helpers/bluesky/__tests__/mocks/metadata.ts
+++ src/helpers/bluesky/__tests__/mocks/metadata.ts
@@ -2,8 +2,9 @@ export const METADATA_MOCK = {
   error: "",
   likely_type: "html",
   url: "https://bsky.app",
-  title: "Bluesky Social",
-  description: "See what's next.",
+  title: "Bluesky",
+  description:
+    "Social media as it should be. Find your community among millions of users, unleash your creativity, and have some fun again.",
   image:
-    "https://cardyb.bsky.app/v1/image?url=https%3A%2F%2Fbsky.app%2Fstatic%2Fsocial-card-default.png",
+    "https://cardyb.bsky.app/v1/image?url=https%3A%2F%2Fbsky.app%2Fstatic%2Fsocial-card-default-gradient.png",
 };
